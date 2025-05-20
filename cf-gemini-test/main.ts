ce9148cba67efe5505938599d2132bd1d4462974
Jasonz17 <13585709483@163.com>
2025-05-20 18:43:56 +0800

    Update main.ts

--- cf-gemini-test/main.ts
+++ cf-gemini-test/main.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
-import { GoogleGenAI, Modality } from "npm:@google/genai"; // 使用正确的库
+import { GoogleGenAI, Modality, createPartFromUri } from "npm:@google/genai"; // 使用正确的库
 import { dirname, fromFileUrl, join } from "https://deno.land/std@0.224.0/path/mod.ts";
 import { serveFile } from "https://deno.land/std@0.224.0/http/file_server.ts";
 import { encodeBase64 } from "https://deno.land/std@0.224.0/encoding/base64.ts";
